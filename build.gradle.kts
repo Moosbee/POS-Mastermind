21d904be3f8e2724759a5311a034f21aad1f2e9e
Moosbee <bernhardr91104@gmail.com>
2023-12-15 17:26:35 +0100

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+}
+
+apply(plugin = "application")
+
+group = "pos.mastermind"
+version = "0.0.1"
+
+configure<JavaApplication> {
+    mainClass = "pos.mastermind.Main"
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "pos.mastermind.Main"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
